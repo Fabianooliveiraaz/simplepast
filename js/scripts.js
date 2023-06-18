c20916829109bdf5e0f9e754b5cb3c60b0a3fcf1
Fabiano Oliveira Azevedo <132237829+Fabianooliveiraaz@users.noreply.github.com>
2023-06-18 12:44:55 -0400

    simplepast

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+        var togo = window.document.getElementById('togo')
+        togo.addEventListener('mouseenter', entrar)
+        togo.addEventListener('mouseout', sair)
+        togo.addEventListener('mouseclick', clicar)
+        
+        function entrar() {togo.innerText='Went'}
+        function sair() {togo.innerText='To Go'}
+        function clicar() {togo.innerText= 'You went.... You did not go.   Did You go?'}
